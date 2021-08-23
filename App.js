6a46fbf689465ae8dabb01a82abdc56a4edaf28a
Osama Azhar <osamaazhar.edu@gmail.com>
2021-08-23 21:19:47 +0500

    React Navigation V6 done

--- App.js
+++ App.js
@@ -1,19 +1,55 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// import { createStackNavigator,createAppContainer } from '@react-navigation/stack';
+import SearchScreen from './src/Screens/SearchScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+// const App = () => {
+//   return (
+//     <View>
+//       <Text>ABC</Text>
+//     </View>
+//   )
+// }
+
+// export default App
+
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Search">
+        <Stack.Screen name="Business Search" component={SearchScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
+// const navigator = createStackNavigator({
+//   Search : SearchScreen
+// },
+// {
+//   initialRouteName: "Search",
+//   defaultNavigationOptions : {
+//     title : "Business Search"
+//   }
+// }
+
+// )
+
+// const Stack = createStackNavigator();
+
+// function MyStack() {
+//   return (
+//     <Stack.Navigator>
+//       <Stack.Screen name="ScreenSearch" component={SearchScreen} />
+//     </Stack.Navigator>
+//   );
+// }
+
+
+// export default createAppContainer(MyStack)
--- src/Screens/SearchScreen.js
+++ src/Screens/SearchScreen.js
@@ -4,7 +4,7 @@ import {View,Text,StyleSheet} from "react-native"
 const SearchScreen = () => {
     return (
         <View>
-            
+            <Text>Search Screen</Text>
         </View>
     )
 }
